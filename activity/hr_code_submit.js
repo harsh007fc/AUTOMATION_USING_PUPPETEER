eb245e14ce182514d98b5ead839ab438320a52d3
Harsh <harsh007fc@gmail.com>
2021-04-11 02:19:28 +0530

    All code submitted done with promises

--- activity/hr_code_submit.js
+++ activity/hr_code_submit.js
@@ -32,73 +32,36 @@ browserPromise.then(function (browserInstance)
     // console.log("login page opened");
 }).then(function ()
 {
-    
-    // let combinedPromise1 = Promise.all([loginPageWillBeClickedPromise,page.waitForNavigation({waitUntil:"networkidle0"})]);
-    // return combinedPromise1;
-
     let loginPageWillBeClickedPromise = page.click("button[data-analytics='LoginPassword']");
     return loginPageWillBeClickedPromise;
 }).then(function ()
 {
     
-    // let clickOnInterviwPrepPromise = page.click(".card-content h3[title='Interview Preparation Kit']");
-    // let warmupChallengeElePromise = page.waitForSelector("a[data-attr1='warmup']",{visible:true});
-    // let combinedPromise2 = Promise.all([clickOnInterviwPrepPromise,page.waitForNavigation({waitUntil:"networkidle0"}),warmupChallengeElePromise]);
-    // return combinedPromise2;
-
-
     let clickIpKit = waitAndClick(".card-content h3[title='Interview Preparation Kit']");
     return clickIpKit;
 })
-// .then(function ()
-//     {
-//         let warmupChallengeElePromise = page.waitForSelector("a[data-attr1='warmup']",{visible:true});
-//         return warmupChallengeElePromise;
-//     }
-// )
+
 .then(function ()
 {
-    // let clickOnWarmupChallenges= page.click("a[data-attr1='warmup']");
-    // let sockmerchantPromise = page.waitForSelector("a[data-attr1='sock-merchant']",{visible:true});
-    // let combinedPromise3 = Promise.all([clickOnWarmupChallenges,page.waitForNavigation({waitUntil:"networkidle0"}),sockmerchantPromise]);
-    // return combinedPromise3;
-
     let warmupClick =  waitAndClick("a[data-attr1='warmup']");
     return warmupClick;
-}).then(function ()
-{
-    return page.url(); //google se mila search krke yeh
-}).then(function(url)
+}).then(function()
 {
     // console.log(url);  //just url print kraya tha
+    let url = page.url();
     let questionObj = codes[0];
     
     //call questionsolver
-    questionSolver(url,questionObj.qname,questionObj.soln);
-})
-
-
-
-
-
-// .then(function () // yeh snippet selector ka wait krne k liye hai
-// {                  ///yeh kaam upr hi kr dete hai
-//     let sockmerchantPromise = page.waitForSelector("a[data-attr1='sock-merchant']",{visible:true});
-//     return sockmerchantPromise;
-// })
-// .then(function () //question pr click
-// {
-//     // let clickOnFirstQues= page.click("a[data-attr1='sock-merchant']"); 
-//     // return clickOnFirstQues;
-
-//     let clickOnQuestionFirst = waitAndClick("a[data-attr1='sock-merchant']");
-//     return clickOnQuestionFirst;
-// })
-// .then(function (){ //yeh hai khud ka promise
-//     let questionSolverPromise = questionSolver();
-//     return questionSolverPromise;
-// })
-.then(function ()
+    let fqsp = questionSolver(url,questionObj.qname,questionObj.soln);
+    for(let i = 1; i < codes.length; i++)
+    {
+        fqsp = fqsp.then(function()
+        {
+            return questionSolver(url,codes[i].qname,codes[i].soln);
+        })
+    }
+    return fqsp;
+}).then(function ()
 {
     console.log("CODE SUBMITTED");
 }).catch(function (err)
@@ -107,8 +70,6 @@ browserPromise.then(function (browserInstance)
 })
 
 
-
-//promise based function -> that do wait and then click
 function waitAndClick(selector)
 {
     return new Promise(function (resolve,reject)
@@ -129,7 +90,7 @@ function waitAndClick(selector)
     })
 }
 
-//question solver function starts her
+//question solver function starts here
 function questionSolver(modulePageUrl,questionName,code)
 {
     return new Promise(function (resolve,reject)
@@ -158,11 +119,6 @@ function questionSolver(modulePageUrl,questionName,code)
           
           return pageClickPromise;
         })
-        // .then(function()
-        // {
-        //     let codeIsTypedIntoInput = typeCodeIntoInput(code);
-        //     return codeIsTypedIntoInput; 
-        // })
         .then(function()  //checkbox pr click
         {
             let inputWillBeClickedPromise = waitAndClick(".custom-checkbox.inline");
@@ -219,77 +175,3 @@ function questionSolver(modulePageUrl,questionName,code)
 }
 
 console.log("after");
-
-
-//-------------------------------------------------------------------------------------------
-//settinghandler fnctn
-//===========================================start=========================================
-// function settingHandler()
-// {
-//     return new Promise(function(resolve,reject)
-//     {
-//         //first wait to load then click 
-//         let settingClickPromise = waitAndClick("button[aria-label='Editor Settings']");
-
-//         settingClickPromise.then(function()
-//         {
-//             let autoCompleteDisablePromise = waitAndClick("button[aria-label='Disable Autocomplete']");
-//             return autoCompleteDisablePromise;
-//         }).then(function()
-//         {
-//             let settingAgainClickPromise = page.click("button[aria-label='Editor Settings']");
-//         }).then(function()
-//         {
-//             resolve();
-//         }).catch(function(err)
-//         {
-//             reject(err);
-//         })
-
-//         //autocomplete disable
-        
-//         //then click again on setting button
-//     })
-// }
-//==========================================END=====================================================
-//---------------------------------------------------------------------
-//this is just a statement
-
-
-
-
-
-
-
-
-
-
-//=========================================
-// .then(function(){
-//     let editorWillBeClickedpromise = page.click(".monaco-editor.no-user-select.vs");
-//     return editorWillBeClickedpromise;
-    
-// }).then(function()
-// {
-//     //yeh bhi google kiya kaise use krte hein ctrl+a in puppeteer
-//     let controlIsHoldPromise = page.keyboard.down("Control");
-//     return controlIsHoldPromise;
-//     // page.keyboard.("A");
-// }).then(function()
-// {
-//     let aIspressPromise = Page.keyboard.press("a");
-//     return aIspressPromise;
-// }).then(function()
-// {
-//     let controlIsReleasedPromise = page.keyboard.up("Control");
-//     return controlIsReleasedPromise;
-// }).then(function()
-// {
-//     let backSpacePressedPromise = page.keyboard.press("Backspace");
-//     return backSpacePressedPromise;
-// }).then(function(){
-
-//     page.type(".inputarea",code);
-// })
-
-//===============================================================
